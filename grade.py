3e7aefa1ea6aad97fa6d64ef880eafc0e075d869
suryansh1404 <83999287+suryansh1404@users.noreply.github.com>
2021-10-09 13:03:36 -0400

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,13 @@
+x=int(input("grade of student" ))
+if x>=90 and x<=100:
+    print("excellent")
+elif x>=80 and x<=90:
+    print("A")
+elif x>=70 and x<=80:
+    print("B")
+elif x>=60 and x<=70:
+    print("c")
+elif x>=50 and x<=60:
+    print("D")
+else:
+    print("fail")
