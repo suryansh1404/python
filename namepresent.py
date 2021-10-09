4d5dae48090b3cfc77188f2c60fdc4083ca1dee4
suryansh1404 <83999287+suryansh1404@users.noreply.github.com>
2021-10-09 13:02:41 -0400

    Create namepresent.py

---
+++ namepresent.py
@@ -0,0 +1,9 @@
+MyList = ["ram","ajay","jay","anish","aman"]
+print("Our List: ", MyList)
+
+
+
+if 'suryansh' not in MyList :
+ print(" item 'suryansh' is not present in the list")
+else:
+ print(" suryansh' is present in the list")
